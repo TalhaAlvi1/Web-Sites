dafcb30971738243d965ccaa5e36298f2d47b847
TalhaAlvi1 <talhahassanalvi786@gmail.com>
2024-10-09 17:00:32 +0500

    image hover

---
+++ index.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'I Love You Too :)';
+
+    // Change to 'Will you marry me?' after a brief delay
+    setTimeout(() => {
+        question.innerHTML = 'Will you marry me?';
+
+        // Update the event listener for the yes button
+        yesBtn.addEventListener('click', () => {
+            question.innerHTML = 'Congratulations!';
+        }, { once: true }); // The "once: true" option ensures this listener runs only once
+    }, 2000); // 2 second delay before showing 'Will you marry me?'
+});
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
